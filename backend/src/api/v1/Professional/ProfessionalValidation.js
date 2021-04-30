16c498897e563424440d6bb7b956ec589b6f4b32
Rafael Lima <rglllima@gmail.com>
2021-04-30 12:34:33 -0300

    Updated Professional Validation

--- backend/src/api/v1/Professional/ProfessionalValidation.js
+++ backend/src/api/v1/Professional/ProfessionalValidation.js
@@ -6,11 +6,13 @@ export default {
     email: yup.string().required().max(60),
     phone: yup.string().nullable().max(60),
     professionId: yup.number().required(),
+    active: yup.boolean()
   }),
   update: yup.object().shape({
     name: yup.string().required().max(60),
     email: yup.string().required().max(60),
     phone: yup.string().nullable().max(60),
     professionId: yup.number().required(),
+    active: yup.boolean()
   })
 }
