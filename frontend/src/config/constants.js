6e759b7cca98de91264f0612c8cde4945374dde6
Rafael Lima <rglllima@gmail.com>
2021-04-30 12:36:03 -0300

    Included Env Vars with Dotenv

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,3 +1,9 @@
+let BASE_URL = process.env.REACT_APP_PROD_BASE_URL;
+
+if (process.env.NODE_ENV !== 'production') {
+  BASE_URL = process.env.REACT_APP_DEV_BASE_URL
+}
+
 export const API = {
-  BASE_URL: 'http://localhost:4000/api/v1',
+  BASE_URL
 };
